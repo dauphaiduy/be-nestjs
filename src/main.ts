348ada4d36f5ac2d9899cd35d4c14c80456b6c1c
duy <khanhduy@lottecnf.com>
2026-05-08 16:33:30 +0700

    init

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       stopAtFirstError: true,
